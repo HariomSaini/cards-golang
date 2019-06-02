41ddd512fcea1e92df9effa51c8b730b5183228d
HariomSaini <hariomsaini0011@gmail.com>
2019-06-02 23:26:52 +0530

    Added more tests

--- deck_test.go
+++ deck_test.go
@@ -7,4 +7,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Not the expected length of deck, But got- %v ", len(d))
 	}
+	if d[0] != "Ace Of Spades" {
+		t.Errorf("Expected Card is Ace of Spades but got %v", d[0])
+	}
+	if d[len(d)-1] != "King Of Clubs" {
+		t.Errorf("Expected Card is King of Clubs but got %v", d[len(d)-1])
+	}
 }
