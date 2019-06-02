12429018f88602da6312b8efa722f0cd4e50e5f6
HariomSaini <hariomsaini0011@gmail.com>
2019-06-02 10:08:16 +0530

    Added a Test Case

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 52 {
+		t.Errorf("Not the expected length of deck, But got- %v ", len(d))
+	}
+}
